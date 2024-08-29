0b48da3f7f6e7e0243cd8cd2d402a06e7c65721e
TheRABU <mdfazlerabbi114@gmail.com>
2024-08-29 13:59:51 +0600

    changed the cors setting by removing / from urls

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://foodie-bite.sifatulrabbi.com/",
-    "https://foodie-bite.web.app/",
-    "https://foodie-app-backend-production.up.railway.app/",
+    "https://foodie-bite.sifatulrabbi.com",
+    "https://foodie-bite.web.app",
+    "https://foodie-app-backend-production.up.railway.app",
   ],
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
   credentials: true,
   optionSuccessStatus: 200,
 };
